363fff1c9fdb0f1b735ab6692dff9544627afed9
fusky(zfmd) <fusk_zfmd@163.com>
2021-04-27 08:35:51 +0800

    add some logic

--- fs17/1_7InPreDataLogFilterFrmData.sh
+++ fs17/1_7InPreDataLogFilterFrmData.sh
@@ -9,15 +9,54 @@
 ############################################################
 #
 
-#grep "\s测风时刻为" *.log |awk -F'，' '{print $(NF-2),$(NF-1),$NF}'|sed 's/。//g'|while read a1 a2 a3 a4 a5 a6 a7
-#grep "\s测风时刻为" *.log |awk -F'，' '{print $(NF-2),$(NF-1),$NF}'|sed 's/。//g'|sed 's/^\s\+//g'|while read a1 a2 a3 a4 a5 a6 a7
+tmpFcbh="011202"
 
-echo "SJRQ,BSWPJFS,BSWPJFX,LSFCBH"
 
-grep "\s测风时刻为" *.log |awk -F'，' '{print $(NF-2),$(NF-1),$NF}'|sed 's/。//g'|sed 's/^\s\+测风时刻为：//g'|sed  's/风速为：//g'|sed  's/风向为：//g'|sed 's///g'|while read a1 a2 a3 a4 a5 a6 
-do
+function F_BSFSFX()
+{
 
-    echo "${a1} ${a2},${a3},${a4},011202"
-done
+    #grep "\s测风时刻为" *.log |awk -F'，' '{print $(NF-2),$(NF-1),$NF}'|sed 's/。//g'|while read a1 a2 a3 a4 a5 a6 a7
+    #grep "\s测风时刻为" *.log |awk -F'，' '{print $(NF-2),$(NF-1),$NF}'|sed 's/。//g'|sed 's/^\s\+//g'|while read a1 a2 a3 a4 a5 a6 a7
+
+    local a1; local a2; local a3; local a4
+    local a5; local a6;
+
+    echo "SJRQ,BSWPJFS,BSWPJFX,LSFCBH"
+
+
+    grep "\s测风时刻为" *.log |awk -F'，' '{print $(NF-2),$(NF-1),$NF}'|sed 's/。//g'|sed 's/^\s\+测风时刻为：//g'|sed  's/风速为：//g'|sed  's/风向为：//g'|sed 's///g'|while read a1 a2 a3 a4 a5 a6 
+    do
+
+        echo "${a1} ${a2},${a3},${a4},${tmpFcbh}"
+    done
+
+    return 0
+}
+
+function F_yggl()
+{
+    local a1; local a2; local a3; local a4
+    local a5; local a6;
+
+    echo "SJRQ,YGGL,FCBH"
+
+
+    grep "\s获取整场功率的运行时间为" *.log|awk -F',' '{print $(NF-2),$(NF-1)}'|awk '{print $(NF-2),$(NF-1),$NF}'|sed 's/获取整场功率的运行时间为：//g;s/有功功率为：//g'|while read a1 a2 a3
+    do
+        echo "${a1} ${a2},${a3},${tmpFcbh}"
+    done
+
+
+    return 0
+}
+
+main()
+{
+    F_BSFSFX >fsfx.csv
+    F_yggl>yggl.csv
+
+}
+
+main
 
 exit 0
